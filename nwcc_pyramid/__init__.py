07d5e02a51b95ffe0112485d96f960651fdb9f60
Chris Closser <chrisclosser1@gmail.com>
2018-04-23 12:52:21 -0700

    serving homepage

--- nwcc_pyramid/__init__.py
+++ nwcc_pyramid/__init__.py
@@ -6,7 +6,7 @@ def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.include('.models')
+    # config.include('.models')
     config.include('.routes')
     config.include('.views')
     config.scan()
--- nwcc_pyramid/views/default.py
+++ nwcc_pyramid/views/default.py
@@ -1,33 +1,22 @@
-from pyramid.response import Response
-from pyramid.view import view_config
+"""Interstate Sales Views."""
 
-from sqlalchemy.exc import DBAPIError
-
+# from pyramid.security import remember, forget
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound
+from pyramid.view import view_config, forbidden_view_config
+# from ..security import is_authenticated
 from ..models import MyModel
+import shutil
+import os
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='home', renderer='../templates/home.jinja2')
+def home_view(request):
+    """Home view."""
+    auth = False
     try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'nwcc_pyramid'}
-
-
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_nwcc_pyramid_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+        auth = request.cookies['auth_tkt']
+    except KeyError:
+        pass
+    return {
+        'auth': auth,
+    }
