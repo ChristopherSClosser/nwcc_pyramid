140eae235fdc2913cc242011f6921b9f5a9a216f
Chris Closser <chrisclosser1@gmail.com>
2018-04-23 17:03:40 -0700

    nav for mobile and gallery slider

---
+++ nwcc_pyramid/static/scripts/app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+$(function(){
+  // Keep track of last scroll
+  var lastScroll = 0;
+  $(window).scroll(function(event){
+    // Sets the current scroll position
+    var st = $(this).scrollTop();
+    // Determines up-or-down scrolling
+    if (st > lastScroll){
+      // function call for downward-scrolling
+      // $('.ckata').hide();
+      // $('.linked').hide();
+      $('header').slideUp('fast');
+    }
+    else {
+      // function call for upward-scrolling
+      $('header').slideDown('fast');
+      // $('.ckata').fadeIn('fast');
+      // $('.linked').fadeIn('fast');
+    }
+    // Updates scroll position
+    lastScroll = st;
+  });
+});
+
+// displays gallery imgs in slide show format
+$(function () {
+  /* time and transition settings */
+  let change_img_time 	= 6000,
+    transition_speed	= 400,
+    simple_slideshow	= $('#slider'),
+    listItems	= simple_slideshow.children('li'),
+    listLen	= listItems.length, i	= 0,
+
+    changeList = function () {
+      listItems.eq(i).fadeOut(transition_speed, function () {
+        i += 1;
+        if (i === listLen) {
+          i = 0;
+        }
+        listItems.eq(i).fadeIn(transition_speed);
+      });
+    };
+
+  listItems.not(':first').hide();
+  setInterval(changeList, change_img_time);
+});
+
+// function for mobile menu on page ready
+$(function(){
+  $('section').hide();
+
+  $('.burger').on('click', function(){
+    if ($('nav').is(':hidden')){
+      $('#main-nav').slideDown('fast');
+    } else {
+      $('#main-nav').slideUp('fast');
+    }
+  });
+
+  // hide nav into burger when width is small
+  $('#home, #about, #events').on('click', function(){
+    if (window.innerWidth <= 739){
+      $('#main-nav').slideUp('fast');
+    }
+  });
+
+
+
+  // displays project list on click for mobile
+  // $('.list-projects').on('click', function(){
+  //   if ($('.dropdown-content').is(':hidden')){
+  //     // $('#project-list').animate({width: 'toggle'},350);
+  //     $('#project-list').slideDown('fast');
+  //   } else {
+  //     $('#project-list').slideUp('fast');
+  //   }
+  // });
+
+  // handles projects to display
+  // $('.dropdown-content').on('click', function(e){
+  //   e.preventDefault();
+  //   $('.github').fadeOut();
+  //   $('.gallery').fadeOut();
+  //   $('.me').fadeOut();
+  //
+  //   if (e.target.id === 'all'){
+  //     $('section').fadeIn();
+  //     $('content').fadeIn();
+  //   }else{
+  //     $('content').hide();
+  //     let $select = (e.target.text);
+  //     projectView.projectFilter($select);
+  //     $('section').fadeIn();
+  //   }
+  //
+  //   $('.dropdown-content').slideUp('fast');
+  //
+  //   if (window.innerWidth <= 739){
+  //     $('#main-nav').slideUp('fast');
+  //   }
+  // });
+});
