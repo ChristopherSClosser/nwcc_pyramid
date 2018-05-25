530c899634a8161df1fa47a1ae390bb263372a53
Chris Closser <chrisclosser1@gmail.com>
2018-05-25 10:46:56 -0700

    worship templated

--- nwcc_pyramid/views/default.py
+++ nwcc_pyramid/views/default.py
@@ -150,6 +150,32 @@ def go_deeper_view(request):
     }
 
 
+@view_config(route_name='worship', renderer='../templates/worship.jinja2')
+def worship_view(request):
+    """Worship view."""
+    auth = False
+    try:
+        auth = request.cookies['auth_tkt']
+    except KeyError:
+        pass
+    query = request.dbsession.query(MyModel)
+    content = query.filter(MyModel.page == 'worship').all()
+    topimg = [item for item in content if item.category == 'topimg']
+    tri_img = [item for item in content if item.category == 'tri_img']
+    quad_info = [item for item in content if item.category == 'quad_info']
+    main = [item for item in content if item.category == 'main']
+    steps = [item for item in content if item.category == 'steps']
+    return {
+        'auth': auth,
+        'content': content,
+        'topimg': topimg[0],
+        'tri_img': tri_img,
+        'quad_info': quad_info,
+        'main': main[0],
+        'steps': steps,
+    }
+
+
 @view_config(route_name='foodbank', renderer='../templates/foodbank.jinja2')
 def foodbank_view(request):
     """Foodbank view."""
