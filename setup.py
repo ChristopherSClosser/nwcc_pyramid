327dbedf8d786e8b36935e396792a66a40c5e2fe
Chris Closser <chrisclosser1@gmail.com>
2019-04-06 09:20:24 -0700

    version update 2.1.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='nwcc_pyramid',
-    version='2.0',
+    version='2.1.3',
     description='nwcc_pyramid',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
